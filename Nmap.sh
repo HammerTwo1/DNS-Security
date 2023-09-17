1d85c1ab27ee72f96f9f9b85f59929280c616c3f
HammerTwo1 <hesamsa45@gmail.com>
2023-09-16 22:02:59 -0500

    Nmap Script has been updated.

--- Nmap.sh
+++ Nmap.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-if [! -f "$1"]; then
+# Check if Nmap is installed
+if ! command -v nmap &> /dev/null; then
+    echo "Nmap is not installed. Please install it first."
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
     echo "Usage: $0 <file>"
     exit 1
 fi
@@ -9,11 +15,17 @@ file="$1"
 while IFS= read -r ip; do
     echo -n "Scanning $ip: "
 
-    nmap -p 53 "$ip" | grep "53/open" > /dev/null
+    nmap -p 53 "$ip" | grep "53/tcp open" > /dev/null
 
-    if [$? -eq 0]; then
+    if [ $? -eq 0 ]; then
         echo "Port 53 open"
     else
-        echo "Port 53 closed"
+    	nmap -Pn 53 "$ip" | grep "53/tcp open" > /dev/null
+
+    	if [$? -eq 0]; then
+    		echo "Port 53 open"
+    	else
+    		echo "Port 53 closed"
+    	fi
     fi
 done < "$file"
