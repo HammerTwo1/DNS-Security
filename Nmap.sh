2cda18958512fc3d61d0de4f3f31d4ec6ed5de6a
HammerTwo1 <hesamsa45@gmail.com>
2023-09-17 16:05:33 -0500

    Updated Nmap script

--- Nmap.sh
+++ Nmap.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
-# Check if Nmap is installed
-if ! command -v nmap &> /dev/null; then
-    echo "Nmap is not installed. Please install it first."
+if [ -z "$2" ]; then
+    echo "Usage: $0 <input_file> <output_file>"
     exit 1
 fi
 
-if [ ! -f "$1" ]; then
-    echo "Usage: $0 <file>"
-    exit 1
-fi
+input_file="$1"
+output_file="$2"
 
-file="$1"
+
+> "$output_file"
+
 while IFS= read -r ip; do
     echo -n "Scanning $ip: "
 
+    # Use Nmap to scan port 53
     nmap -p 53 "$ip" | grep "53/tcp open" > /dev/null
 
+    # Check the exit status of the previous command
     if [ $? -eq 0 ]; then
-        echo "Port 53 open"
+        echo "Port 53 is open for $ip"
+        echo "$ip" >> "$output_file"
     else
-    	nmap -Pn 53 "$ip" | grep "53/tcp open" > /dev/null
+        nmap -Pn 53 "$ip" | grep "53/tcp open" > /dev/null
 
-    	if [$? -eq 0]; then
-    		echo "Port 53 open"
-    	else
-    		echo "Port 53 closed"
-    	fi
+        if [ $? -eq 0 ]; then
+            echo "Port 53 is open for $ip"
+            echo "$ip" >> "$output_file"
+        else
+            echo "Port 53 is closed for $ip"
+        fi
     fi
-done < "$file"
+done < "$input_file"
+
+echo "Results saved in $output_file"
+
