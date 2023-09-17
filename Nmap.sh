c10ca9399319459f843fdfb8209e603d2386a922
Hesam Sadeghian <65884061+HammerTwo1@users.noreply.github.com>
2023-09-16 20:50:50 -0500

    Update Nmap.sh

--- Nmap.sh
+++ Nmap.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+if [! -f "$1"]; then
+    echo "Usage: $0 <file>"
+    exit 1
+fi
+
+file="$1"
+while IFS= read -r ip; do
+    echo -n "Scanning $ip: "
+
+    nmap -p 53 "$ip" | grep "53/open" > /dev/null
+
+    if [$? -eq 0]; then
+        echo "Port 53 open"
+    else
+        echo "Port 53 closed"
+    fi
+done < "$file"
