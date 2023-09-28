89a57288c84249ddbc19d7e29775b4e7d63ed582
Bishop <bishop@localhost.localdomain>
2023-09-28 06:17:19 -0500

    Updated Open port list

--- whoisScript.sh
+++ whoisScript.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+output_file="whois.txt"
+
+declare -A ip_seen
+
+start_datetime=$(date "+%Y-%m-%d %H:%M:%S")
+echo "Start Date and Time: $start_datetime" >> "$output_file"
 
 for ip in $(cat OpenPort.txt); do
-    whois $ip | grep 'OrgName' >> whois.txt
+    # Check if the IP address is already seen, and skip if it's a duplicate
+    if [[ -n "${ip_seen[$ip]}" ]]; then
+        continue
+    fi
+
+    org_name=$(whois "$ip" | awk -F':' '/^OrgName/{print $2}' | tr -d '[:space:]')
+    echo "IP: $ip, OrgName: $org_name" >> "$output_file"
+
+    # Mark the IP as seen
+    ip_seen["$ip"]=1
 done
 
+end_datetime=$(date "+%Y-%m-%d %H:%M:%S")
+echo "End Date and Time: $end_datetime" >> "$output_file"
