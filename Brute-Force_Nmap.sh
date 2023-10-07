01f2ab06bbd4aef54f827970cf7f8dd4201f53cd
Bishop <bishop@localhost.localdomain>
2023-10-07 13:38:31 -0500

    fixed some bugs

--- Brute-Force_Nmap.sh
+++ Brute-Force_Nmap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 port="53"
 
 open_port_ips=()
@@ -12,19 +11,20 @@ start_time=$(date +"%Y-%m-%d %H:%M:%S")
 echo "Script started at: $start_time"
 
 # Loop through subnets from 0.0.0.0/24 to 255.255.255.0/24
-for subnet in {0..63}.{1..63}.{2..63}.0/24; do
-    ip_range="0.1.2.0/24"
+for subnet in {8..255}.{8..255}.{8..255}.0/24; do
+    ip_range="$subnet"
 
-    ip_list=$(nmap -sL $subnet | awk '/Nmap scan report for/{print $NF}')
+    # Use nmap -sL to list IP addresses in the subnet
+    ip_list=$(nmap -sL $ip_range | awk '/Nmap scan report for/{gsub(/\(|\)/, "", $NF); print $NF}')
 
     for current_ip in $ip_list; do
-
         current_time=$(date +"%Y-%m-%d %H:%M:%S")
         echo "[$current_time] Checking IP: $current_ip"
        
-        nmap -p "$port" -sU "$current_ip" | grep "53/udp open" > /dev/null
+       
+        # Use nmap to check if port 53 UDP is open
+        nmap -p 53 -sU "$current_ip" | grep "53/udp open" > /dev/null
 
-     
         if [ "$?" -eq 0 ]; then
             open_port_ips+=("$current_ip")
             echo "Open Port 53 found at: $current_ip"  
@@ -33,7 +33,7 @@ for subnet in {0..63}.{1..63}.{2..63}.0/24; do
         ((iteration++))
 
         # Every thousand iterations, save results to a file
-        if [ "$(($iteration % 1000))" -eq 0 ]; then
+        if [ "$(($iteration % 10))" -eq 0 ]; then
             output_file="open_port_ips_${iteration}.txt"
             echo "IPs with Port 53 Open (UDP):" > "$output_file"
             for ip in "${open_port_ips[@]}"; do
