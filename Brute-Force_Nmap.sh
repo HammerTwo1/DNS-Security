a71f924c4c7f8dd0e66dfe814cf51d6c5fec24e0
Bishop <bishop@localhost.localdomain>
2023-10-07 09:12:23 -0500

    The Brute-Force_Nmap.sh is the complete and final script.

--- Brute-Force_Nmap.sh
+++ Brute-Force_Nmap.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-start_ip="0.0.0.0"
-end_ip="255.255.255.255"
+
 port="53"
 
 open_port_ips=()
@@ -9,17 +8,21 @@ open_port_ips=()
 # Initialize a counter to track iterations
 iteration=0
 
+start_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Script started at: $start_time"
+
 # Loop through subnets from 0.0.0.0/24 to 255.255.255.0/24
-for subnet in {0..255}.{0..255}.{0..255}.0/24; do
-    ip_range="0.0.0.0/24"
+for subnet in {0..63}.{1..63}.{2..63}.0/24; do
+    ip_range="0.1.2.0/24"
 
     ip_list=$(nmap -sL $subnet | awk '/Nmap scan report for/{print $NF}')
 
     for current_ip in $ip_list; do
-        echo "Checking IP: $current_ip"
 
+        current_time=$(date +"%Y-%m-%d %H:%M:%S")
+        echo "[$current_time] Checking IP: $current_ip"
        
-        nmap -p "$port" -sU -sT "$current_ip" | grep "53/tcp open\|53/udp open" > /dev/null
+        nmap -p "$port" -sU "$current_ip" | grep "53/udp open" > /dev/null
 
      
         if [ "$?" -eq 0 ]; then
@@ -32,10 +35,10 @@ for subnet in {0..255}.{0..255}.{0..255}.0/24; do
         # Every thousand iterations, save results to a file
         if [ "$(($iteration % 1000))" -eq 0 ]; then
             output_file="open_port_ips_${iteration}.txt"
-            echo "IPs with Port 53 Open (TCP/UDP):" > "$output_file"
+            echo "IPs with Port 53 Open (UDP):" > "$output_file"
             for ip in "${open_port_ips[@]}"; do
                 echo "$ip" >> "$output_file"
-                echo "Open Port 53 (TCP/UDP): $ip" >> "$output_file"
+                echo "Open Port 53 (UDP): $ip" >> "$output_file"
             done
             echo "Results saved in $output_file"
             open_port_ips=()  # Clear the list for the next batch
@@ -45,12 +48,15 @@ done
 
 # Save any remaining results
 output_file="open_port_ips_remaining.txt"
-echo "IPs with Port 53 Open (TCP/UDP):" > "$output_file"
+echo "IPs with Port 53 Open (UDP):" > "$output_file"
 for ip in "${open_port_ips[@]}"; do
     echo "$ip" >> "$output_file"
-    echo "Open Port 53 (TCP/UDP): $ip" >> "$output_file"
+    echo "Open Port 53 (UDP): $ip" >> "$output_file"
 done
 echo "Results saved in $output_file"
+
+end_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Script completed at: $end_time"
 
 echo "Scan completed."
 
